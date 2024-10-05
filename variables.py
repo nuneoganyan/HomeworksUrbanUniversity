c77179c9200bc97fc4e9c7fe0e1d9941a82cba05
nuneoganyan <oganyanune@gmail.com>
2024-10-05 22:40:41 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,6 @@
+number_of_complited_homeworks = 12
+number_of_spent_hours = 1.5
+name_of_the_Course = 'Python'
+time_per_task = number_of_spent_hours / number_of_complited_homeworks
+print('Курс:', name_of_the_Course,', всего задач:', number_of_complited_homeworks, ', зарачено часов:',
+      number_of_spent_hours,', среднее время выполнения:', time_per_task, 'часа')
