30afc107b1560b0b27a95bd7375497e0f2355b84
nuneoganyan <oganyanune@gmail.com>
2024-10-05 22:18:16 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = 'Nune'
+print('Name: ' + name)
+age = 28
+print ('Age: ' + str(age))
+age = age + 50
+print('New Age: ' + str(age))
+is_student = True
+print('Is Student: ' + str(is_student))
