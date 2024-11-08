ebb8e16f34789195e762f02797fa92cabbc24b64
nuneoganyan <oganyanune@gmail.com>
2024-11-08 20:31:15 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+import fake_math as fake
+import true_math as true
+
+result1 = fake.divide(45, 9)
+result2 = fake.divide(45, 0)
+result3 = true.divide(23, 23)
+result4 = true.divide(23, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+
---
+++ true_math.py
@@ -0,0 +1,6 @@
+import math
+def divide(first, second) :
+    if second == 0 :
+        return math.inf
+    else :
+        return first / second
